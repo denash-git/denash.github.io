6943ef29d1222cc931502a27aaa4ac1ab3474492
denash-tm <denash0@gmail.com>
2017-05-26 20:06:00 +0300

    мой первый коммит

---
+++ main.js
@@ -0,0 +1,12 @@
+var value = "Hello javascript"
+var result = 0
+
+result = 10 % 4
+console.log(result)
+//объекты для взамиодействия с браузером и их методы
+/*
+  проверка комента в 2 строки
+*/
+document.write('Helllo Dima!' + '</br>')
+document.write(value)
+console.log(document.getElementById('1'))
