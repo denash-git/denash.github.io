0e4d81366a1325dd23dfd43bf20e3407ec9ae167
denash-tm <denash0@gmail.com>
2017-06-16 16:20:39 +0300

    дом_5

--- dom_5/js/machine.js
+++ dom_5/js/machine.js
@@ -30,16 +30,6 @@ var coffee = [
     defoltSugar: 5
   },
   {
-    name: "Ристретто",
-    price: 79,
-    defoltSugar: 6
-  },
-  {
-    name: "Мокачино",
-    price: 56,
-    defoltSugar: 4
-  },
-  {
     name: "Лунго",
     price: 64,
     defoltSugar: 5
@@ -56,17 +46,145 @@ var coffee = [
   },
 ];
 
+var cash = [
+    {
+      coin: 1,
+    },
+    {
+      coin: 2,
+    },
+    {
+      coin: 5,
+    },
+    {
+      coin: 10,
+    },
+    {
+      coin: 50,
+    },
+    {
+      coin: 100,
+    },
+    {
+      coin: 500,
+    },
+  ];
+
 function Mashine() {
-  this.Drink = "";
-  this.CoffeeList = coffee;
-  this.RenderList = function () {
-    var a = this.CoffeeList;
-    container = document.getElementById("coffeebutton");
-    for (var i = 0; i < a.length; i++) {
+    var money = 0;          // внесено на депозит кофе
+    var delivery = 0;      // сдача
+    var idDrink = "false";   // id выбранного кофе
+    var priceDrink = 0;    // цена напитка
+    var nameDrink = "";     // наименование напитка
+    var offMoney = true;    // on/off кошелька
+    var sugar = 0;           //сахар
+
+  //получаем количество депозита
+  this.getMoney = function () {
+    return money;
+  }
+
+  // добавление на депозит
+  this.setMoney = function (give) {
+    if (offMoney == true) {
+      money = +money + +give;
+    }
+    console.log(money)
+    this.display()
+  }
+
+  //изменение сахара
+  this.changeSugar = function (btn) {
+    if (idDrink != "false") {
+      
+      if (btn == "max") {
+        sugar = sugar + 1;
+      };
+      if (btn == "min") {
+        sugar = sugar - 1;
+      };
+      line2 = "Сахар: " + sugar + " ложек :-)";
+      print();
+    }
+  }
+  // логика сообщений пользователю
+  this.display = function() {
+    if (money == 0 && idDrink == "false") {
+      line1 = "Мы приветствуем Вас !";
+      line2 = "выберите напиток или";
+      line3 = "внесите наличные";
+    }
+    if (money != 0 && idDrink == "false") {
+      line1 = "Вы внесли сумму: " + money + " руб.";
+      line2 = "выберите напиток ....";
+      line3 = "";
+    }
+    if (money == 0 && idDrink != "false") {
+      line1 = "Вы выбрали напиток: " + nameDrink;
+      line2 = "цена напитка: " + priceDrink + " руб.";
+      line3 = "внесите наличные";
+    }
+    if (money != 0 && idDrink != "false" && money < priceDrink) {
+      line1 = "Вы выбрали напиток: " + nameDrink;
+      line2 = "цена напитка: " + priceDrink + " руб.";
+      line3 = "осталось внести: " + (priceDrink - money) + " руб."; 
+    }
+    if (money >= priceDrink && idDrink != "false") {
+      setTimeout(function() {
+        line1 = "Ваш напиток готов ! ";
+        line2 = "";
+        line3 = "ждём Вас еще ... ";
+        print();
+      }, 5000);
+        line1 = "Ваш напиток готовиться ! ";
+        line2 = "Ваша сдача: " + (money - priceDrink) + " руб.";
+        line3 = "минуточку ... ";
+        money = 0;
+        idDrink = "false";
+    }
+    print();
+
+  }
+
+  // вывод подг. текста на экран
+  function print () {
+    document.getElementById('line1').textContent = line1;
+    document.getElementById('line2').textContent = line2;
+    document.getElementById('line3').textContent = line3;
+  }
+
+  // выводим список купюр
+  this.renderCash = function () {
+    container = document.getElementById("pocket");
+    container.innerHTML ="";
+    for (var i = 0; i < cash.length; i++) {
       container.innerHTML += (
-        `<li value=${a[i].price} class="CoffeeItem" data-name=${a[i].name}>
-        ${a[i].name}
+        `<li class="MoneyItem" data-name=${cash[i].coin}>
+        ${cash[i].coin} &#8381;
         </li>`
+      )
+    }
+
+  }
+  
+  //позиция напитка в массиве, если выбран, цена и имя
+  this.idDrink = function (give) {
+    console.log(give)
+    idDrink = give;
+    priceDrink = coffee[give].price;
+    nameDrink = coffee[give].name;
+    sugar = coffee[give].defoltSugar
+    this.display()
+  }
+
+  // форм списка кофе
+  this.renderList = function () {  
+    container = document.getElementById("coffeebutton");
+    container.innerHTML ="";
+    for (var i = 0; i < coffee.length; i++) {
+      container.innerHTML += (
+        `<li class="CoffeeItem" data-name=${i}><span>${coffee[i].price} &#8381;</span>
+            ${coffee[i].name}</li>`
       )
     }
 
--- dom_5/js/main.js
+++ dom_5/js/main.js
@@ -1,31 +1,78 @@
-var newPocket = new Pocket();  //создание кошелька
-var NewMashine = new Mashine(); //создание машины
-var newDisplay = new PrintDisplay(); //создание дисплея
+//создание машины
+var newMashine = new Mashine();
 
+// форм лист наличных
+newMashine.renderCash();
 
+// форм лист кофе
+newMashine.renderList();
 
-// формируется лист наличных
-newPocket.RenderCash();
+// приглашение
+newMashine.display();
 
 // внесение денег
-var ChoiceMoney = document.getElementsByClassName('MoneyItem');
-for (var i = 0; i < ChoiceMoney.length; i++) {
-  ChoiceMoney[i].addEventListener('click', function(e) {
-    newPocket.money = +newPocket.money + +e.target.dataset.name;
-    console.log(newPocket.money);
-    console.log(newDisplay.Print(1));
-  })
-};
-
-// форм Лист кофе
-NewMashine.RenderList();
+function cashup() {
+var choiceMoney = document.getElementsByClassName('MoneyItem');
+for (var i = 0; i < choiceMoney.length; i++) {
+  choiceMoney[i].addEventListener('click', function(e) {
+    newMashine.setMoney(+e.target.dataset.name);
+   })
+};	
+}
+cashup();
 
 // выбор напитка
-var ChoiceDrink = document.getElementsByClassName('CoffeeItem');
-for (var i = 0; i < ChoiceDrink.length; i++) {
-  ChoiceDrink[i].addEventListener('click', function(e) {
-    NewMashine.Drink = e.target.dataset.name;
-    console.log(NewMashine.Drink);
+function listUp(){
+	var choiceDrink = document.getElementsByClassName('CoffeeItem');
+for (var i = 0; i < choiceDrink.length; i++) {
+  choiceDrink[i].addEventListener('click', function(e) {
+    newMashine.idDrink(+e.target.dataset.name);		//запомнинаем напиток, позиция в массиве
+    console.log(+e.target.dataset.name)
   })
+};
 }
+listUp();
+
+//событие добавления напитка вручную
+var dn = document.getElementById('drink-name');
+var dp = document.getElementById('drink-price');
+document.getElementById('drink-add').addEventListener('click', function() {
+	if (dn.value == "" || dp.value == 0) {
+		alert ("Введите пожалуйста все значения !");
+	} else {
+			coffee.push({
+			name: dn.value,
+    		price: dp.value,
+    		defoltSugar: 3
+			});
+		};
+		var newMashine = new Mashine();
+		newMashine.renderList();
+		listUp();
+});
+
+//событие добавления купюры вручную
+var сurr = document.getElementById('curr');
+document.getElementById('curr-add').addEventListener('click', function() {
+	if (curr.value > 101 || curr.value == 0) {
+		alert ("Введите пожалуйста корректное значение !   от 1 до 100");
+	} else {
+			cash.push({
+			coin: curr.value,
+			});
+		};
+		var newMashine = new Mashine();
+		newMashine.renderCash();
+		cashup();
+});
+
+// изменение сахара
+document.getElementById('btn+').addEventListener('click', function () {
+	newMashine.changeSugar("max");
+	console.log ("max");
+})
+document.getElementById('btn-').addEventListener('click', function () {
+	newMashine.changeSugar("min");
+	console.log ("min");
+})
 
